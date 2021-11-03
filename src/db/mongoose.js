0879bf2e9ac11fc4bd22774fa5f290d37482ef54
Dylan <dflauzon@gmail.com>
2021-11-03 09:09:46 -0400

    Updated models to include basic data validation and sanitization, some custom and some with the help of npm validator.

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,47 +1,93 @@
 const mongoose = require('mongoose')
+const validator = require('validator')
 
-mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
-    useNewUrlParser: true,
-    useCreateIndex: true
-})
+mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api')
 
+// mongoose models
 const User = mongoose.model('User', {
     name: {
-        type: String
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+            if(!validator.isEmail(value)){
+                throw new Error('Email is invalid')
+            }
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 7,
+        validate(value) { 
+            if(value.toLowerCase().includes('password')){
+                throw new Error('Password cannot contain the the word password.')
+            }
+        }
     },
     age: {
-        type: Number
+        type: Number,
+        default: 0,
+        validate(value) {
+            if(value < 0) {
+                throw new Error('Age must be a positive number.')
+            }
+        }
     }
 })
-
-// const me = new User({
-//     name: 'Dylan',
-//     age: 32
-// })
-
-// me.save().then( () => {
-//     console.log(me)
-// }).catch((error) => {
-//     console.log('Error!', error)
-// })
 
 const Task = mongoose.model('Tasks', {
     description: {
-        type: String
+        type: String,
+        required: true,
+        trim: true
     },
     completed: {
-        type: Boolean
+        type: Boolean,
+        default: false
     }
 })
 
-const clean = new Task({
-    description: 'Clean the house',
-    completed: false
+const me = new User({
+    name: '   Dylan ',
+    email: '  DFLAUZON@GMAIL.COM',
+    password: 'PASSWORD123'
 })
 
-clean.save().then(() => {
-    console.log(clean)
+me.save().then( () => {
+    console.log(me)
 }).catch((error) => {
-    console.log('Error', error)
+    console.log('Error!', error)
 })
+
+
+
+// const clean = new Task({
+//     description: 'Clean the house',
+//     completed: false
+// })
+
+// clean.save().then(() => {
+//     console.log(clean)
+// }).catch((error) => {
+//     console.log('Error', error)
+// })
+ 
+// const task = new Task({
+//     description: 'Walk the dog',
+//     completed: true
+// })
+
+// task.save().then(() => {
+//     console.log(task)
+// }).catch((error) => {
+//     console.log(error)
+// })
 
