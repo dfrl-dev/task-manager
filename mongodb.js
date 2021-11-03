f85aa038436731540eaa4f99c3182415255ce45f
Dylan <dflauzon@gmail.com>
2021-11-02 20:00:30 -0400

    Setting up mongoose and playing around with basics.

--- mongodb.js
+++ mongodb.js
@@ -1,10 +1,11 @@
 //CRUD create read update delete
 
-const mongodb = require('mongodb')
-const MongoClient = mongodb.MongoClient
+
+const { MongoClient, ObjectID } = require('mongodb')
 
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
+
 
 MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
     if(error){
@@ -12,55 +13,22 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
     } 
 
     const db = client.db(databaseName)
-    
-    // db.collection('users').insertOne({
-    //     name: 'Dylan',
-    //     age: '32'
-    // }, (error, result) => {
-    //     if(error) {
-    //         return console.log('Unable to insert user.')
-    //     }
 
-    //     console.log(result.ops)
-    // })
+   //  db.collection('users').deleteMany({
+   //       age: '32'
+   //  }).then((result) => {
+   //       console.log(result)
+   //  }).catch((error) => {
+   //       console.log(error)
+   //  })
 
-    // db.collection('users').insertMany([
-    //     {
-    //         name: 'Franklin',
-    //         age: '7'
-    //     },
-    //     {
-    //         name: 'Rachelle',
-    //         age: '32'
-    //     }
-    // ], (error, result) => {
-    //     if(error){
-    //         return console.log('failed to insert')
-    //     }
-
-    //     console.log(result.ops)
-    // })
-
-    db.collection('tasks').insertMany([
-        {
-            description: 'Give the dog a bath',
-            completed: false
-        },
-        {
-            description: 'Snuggle Rachelle',
-            completed: false
-        },
-        {
-            description: 'Burn out from over studying',
-            completed: true
-        }
-    ], (error, result) => {
-        if(error) {
-            return console.log('failed to insert')
-        }
-
-        console.log(result.ops)
-    })
+   db.collection('tasks').deleteOne({
+      description: 'Give the dog a bath'
+   }).then((result) => {
+      console.log(result.deletedCount)
+   }).catch((error) => {
+      console.log(error)
+   })
 
 
-})
+ })
