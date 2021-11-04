47d594be73ec937d93b6399296ef63d76fc697ea
Dylan <dflauzon@gmail.com>
2021-11-04 10:55:01 -0400

    Added routes for updating and deleting data.

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 3000
 
 app.use(express.json())
 
-//CREATE ENDPOINTS
+//CREATE ROUTES
 
 
 app.post('/users', async (req, res) => {
@@ -33,7 +33,7 @@ app.post('/tasks', async (req, res) => {
     }
 })
 
-//READ ENDPOINTS
+//READ ROUTES
 app.get('/users', async (req, res) => {
     try {
         const users = await User.find({})
@@ -74,6 +74,79 @@ app.get('/tasks/:id', async (req, res) => {
         if(!task){
             return res.status(404).send()
         }
+        res.send(task)
+    } catch(e) {
+        res.status(500).send()
+    }
+})
+
+//UPDATE ROUTES
+app.patch('/users/:id', async (req, res) => {
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['name', 'email', 'password', 'age']
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+
+    if (!isValidOperation){
+        return res.status(400).send({error: 'Invalid updates!'})
+    }
+    
+    try{
+        const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true})
+
+        if(!user){
+            return res.status(404).send()
+        }
+
+        res.send(user)
+    } catch(e){
+        res.status(400).send(e)
+    }
+})
+
+app.patch('/tasks/:id', async (req, res) => {
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['description', 'completed']
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+
+    if(!isValidOperation){
+        return res.status(400).send({error: 'Invalid updates!'})
+    }
+
+    try {
+        const task = await Task.findByIdAndUpdate(req.params.id, req.body, {new: true, runValidators: true})
+
+        if(!task){
+            return res.status(404).send({error: 'Task not found!'})
+        }
+        res.send(task)
+    } catch(e) {
+        res.status(400).send(e)
+    }
+})
+
+//DELETE ROUTES
+app.delete('/users/:id', async (req, res) => {
+    try{
+        const user = await User.findByIdAndDelete(req.params.id)
+
+        if(!user){
+            return res.status(404).send({ error: 'User not found' })
+        }
+
+        res.send(user)
+    } catch(e) {
+        res.status(500).send()
+    }
+})
+
+app.delete('/tasks/:id', async (req, res) => {
+    try{
+        const task = await Task.findByIdAndDelete(req.params.id)
+
+        if(!task){
+            return res.status(404).send({ error: 'Task not found' })
+        }
+
         res.send(task)
     } catch(e) {
         res.status(500).send()
